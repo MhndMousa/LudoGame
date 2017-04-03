d937707a67e143023ca71f53e2e2934dc2a8380a
Mhnd Mousa <mhndmsa@gmail.com>
2017-04-03 11:25:04 -0400

    First commit

---
+++ Assets/Waypoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+
+	public static Transform[] points;
+
+	// Use this for initialization
+	void Awake(){
+		points = new Transform[transform.childCount];
+		for (int i = 0; i < points.Length; i++) {
+			points[i] = transform.GetChild (i);
+		}
+	}
+
+	void Update() {
+		print (Input.GetKey("a"));
+		if (Input.GetKeyDown(KeyCode.Space))
+			print("space key was pressed");
+
+	}
+
+}
